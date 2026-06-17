c8deaf162ed03d5117f3fe74217b0d479495cc5b
wuxianggujun <3344207732@qq.com>
2026-06-17 23:53:14 +0800

    test: default schema approval test paths

--- test/sync_project_template_schema_approval_test.ps1
+++ test/sync_project_template_schema_approval_test.ps1
@@ -7,6 +7,14 @@ param(
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
+if (-not $RepoRoot) {
+    $RepoRoot = [System.IO.Path]::GetFullPath((Join-Path $PSScriptRoot ".."))
+}
+
+if (-not $WorkingDir) {
+    $WorkingDir = Join-Path $RepoRoot "build\sync_project_template_schema_approval_test"
+}
+
 function Assert-True {
     param([bool]$Condition, [string]$Message)
     if (-not $Condition) { throw $Message }
